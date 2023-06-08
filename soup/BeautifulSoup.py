4abde08e9c0f699e51a6c7a3612727a11f1f9908
sugarbayar123 <128152928+sugarbayar123@users.noreply.github.com>
2023-06-08 17:10:26 +0200

    Update BeautifulSoup.py

--- soup/BeautifulSoup.py
+++ soup/BeautifulSoup.py
@@ -1,4 +1,3 @@
-
 from urllib import request
 from bs4 import BeautifulSoup as BS
 import re
@@ -7,11 +6,11 @@ import pandas as pd
 
 start_time=time.time()
 
-url='https://concertful.com/area/poland/'
+url = 'https://concertful.com/area/poland/'
 html = request.urlopen(url)
 bs = BS(html.read(), 'html.parser')
 tags = bs.find_all('a')
-tags=tags[3:103]
+tags = tags[3:103]
 links = ['https://concertful.com/' + tag['href'] for tag in tags]
 
 df = pd.DataFrame({'Performer':[], 'Venue':[],'Address':[],'Date':[],'Genre':[]})
@@ -21,13 +20,14 @@ for link in links:
     Performer = bs.find('span',{'class':'performers'}).find('a').text
     Venue = bs.find('span',{'class':'venue_name'}).text
     Address = bs.find('span',{'class':'address'}).find('a').text
-    Date=bs.find('th',string='Date:').next_sibling.next_sibling.text.strip()
-    Date=Date.replace('\t', '')
-    Date=Date.replace('\n', '')
+    Date = bs.find('th',string='Date:').next_sibling.next_sibling.text.strip()
+    Date = Date.replace('\t', '')
+    Date = Date.replace('\n', '')
     Genre = bs.find('th',string='Genre:').next_sibling.next_sibling.text
     add = {'Performer':Performer, 'Venue':Venue,'Address':Address,'Date':Date,'Genre':Genre}
-    df = df.append(add, ignore_index = True)
-df.to_csv('df_beautifulsoup.csv') 
+    # df = df.append(add, ignore_index = True)
+    df = pd.concat([df, pd.DataFrame(add, index=[0])], ignore_index=True)
+df.to_csv('df_beautifulsoup.csv')
 
 end_time=time.time()
 duration=end_time-start_time
